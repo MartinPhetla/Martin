3ce6a12cd9c96aafdbb54bde50a64173ff660956
Martin Phetla <90064605+MartinPhetla@users.noreply.github.com>
2023-12-11 14:48:00 +0200

    Update script.js
    added the script to handle the new loading section

--- script.js
+++ script.js
@@ -12,8 +12,46 @@ function nextSlide() {
     showSlide(currentSlide);
 }
 
-// Initial setup
-showSlide(currentSlide);
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the loading text element
+  const loadingText = document.getElementById('loadingText');
 
-// Automatic slide change every 5 seconds
-setInterval(nextSlide, 2000);
+  // Split the text content into characters
+  const characters = loadingText.innerHTML.split('');
+
+  // Clear the original text content
+  loadingText.innerHTML = '';
+
+  // Iterate over each character and create a span for it
+  characters.forEach((char, index) => {
+    const charElement = document.createElement('span');
+    charElement.innerHTML = char;
+
+    // Apply animation delay for each character
+    charElement.style.animation = `bounceFadeIn 1s steps(20, end) ${index * 0.1}s forwards`;
+
+    // Append the character to the loading text element
+    loadingText.appendChild(charElement);
+  });
+
+  // After the animation, execute the blog slider setup
+  const animationDuration = characters.length * 0.1 * 1000; // total animation duration in milliseconds
+
+  setTimeout(() => {
+    // Hide the loading section
+    document.querySelector('.loading-container').style.display = 'none';
+
+    // Scroll to the "Home" section with a smooth animation
+    document.getElementById('Home').scrollIntoView({
+      behavior: 'smooth'
+    });
+
+    // Initial setup for the blog slider
+    showSlide(currentSlide);
+
+    // Automatic slide change every 5 seconds
+    setInterval(() => {
+      nextSlide();
+    }, 2000);
+  }, animationDuration);
+});
